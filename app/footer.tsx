48f9b5e7102bf42a91007a8b2dc0fea1609202fd
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-03 11:50:17 -0800

    Added a Footer component with business name and copyright info.

---
+++ app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-4">
+      <small>© 2025 Whole Presence Counseling LLC</small>
+    </footer>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Header from '@/app/header'
+import Footer from '@/app/footer'
 
 const geist = Geist({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="m-8">
 	<Header />
         {children}
+	<Footer />
       </body>
     </html>
   );
