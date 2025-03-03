d6f60d0da0303812e55091ea69aec8eaa5941d4f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-03 11:39:26 -0800

    Updated the Header component with a logo and business name.

--- app/header.tsx
+++ app/header.tsx
@@ -1,5 +1,15 @@
+import Image from 'next/image'
+import logo from '@/public/wpc-logo.svg'
+
 export default function Header() {
   return (
-    <h1 className="">Hello World!</h1> 
+    <header className="flex items-center gap-4 mb-4">
+      <Image
+	src={logo}
+	alt='Logo of Whole Presence Counseling'
+	className="w-12"
+      />
+      <h1 className="text-2xl font-bold">Whole Presence Counseling</h1>
+    </header>
   )
 }
