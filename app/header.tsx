ba626617fff2b174ca382ffb225ba976fd8f8770
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-03 11:52:29 -0800

    Updated alt text of logo.

--- app/header.tsx
+++ app/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="flex items-center gap-4 mb-4">
       <Image
 	src={logo}
-	alt='Logo of Whole Presence Counseling'
+	alt='Logo of Whole Presence Counseling, which resembles a hand illustration of a thick, imperfect black circle.'
 	className="w-12"
       />
       <h1 className="text-2xl font-bold">Whole Presence Counseling</h1>
