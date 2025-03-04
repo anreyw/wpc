8af69b81c4b687c83025ae4c61992b82b1433cf1
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-03 22:37:50 -0800

    Fixed links and added styling to links.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-export default function Main() {  
+export default function Main() {
+  const linkStyle = "text-sky-800 underline decoration-dotted decoration-2 underline-offset-4 hover:bg-sky-50"
+  
   return (
     <main className="grid md:grid-cols-2 gap-8">
       <section className="flex flex-col gap-4">
 	<h2 className="text-xl font-semibold">About</h2>
-	<p> My name is Anrey Wang, and I am a Licensed Professional Counselor. I provide existential therapy for adult individuals in Oregon, via secure video conferencing.</p>
+	<p> My name is Anrey Wang, and I am a <a href="https://www.counseling.org/mental-health-counseling/what-is-counseling" target="_blank" className={linkStyle}>Licensed Professional Counselor</a>. I provide existential therapy to adult individuals in Oregon, via secure video conferencing.</p>
 	<p> I practice with authenticity, discerning attention, and a deep commitment to counseling excellence. I also identify as intellectually gifted and may be a good fit for other intellectually gifted individuals. </p>
       </section>
       
@@ -15,20 +17,16 @@ export default function Main() {
 
       <section className="flex flex-col gap-4">
 	<h2 className="text-xl font-semibold">Terms</h2>
-	<p> By law, clients have a right to a good faith estimate of expected costs at scheduling and upon request.</p>
-	<p>Clients are also subject to certain <a href="#" target="_blank">privacy practices</a>.</p>
-	<p>See here for my professional disclosure and complete terms and conditions.</p>
+	<p>Clients are subject to certain <a href="#" target="_blank" className={linkStyle}>privacy practices</a> and must give informed consent to my practice's <a href="" target="_blank" className={linkStyle}>terms and conditions</a> to receive services.</p>
+	<p>By law, clients are protected from <a href="https://www.cms.gov/medical-bill-rights" target="_blank" className={linkStyle}>surprise medical bills</a> and have the right to receive a good faith estimate of expected service costs at the time of scheduling and upon request.</p>
       </section>
 
       <section className="flex flex-col gap-4">
 	<h2 className="text-xl font-semibold">Contact</h2>
 	<p><strong>I am not currently accepting new clients.</strong></p>
 	<p>If necessary, I can be reached by email at anrey<span className="hidden">I</span>@whole<span className="hidden">love</span>presencecounseling<span className="hidden">spam</span>.com.</p>
-	<p>If you are in a crisis, please <em>do not</em> contact me. Instead call or text <a href="tel:988">9-8-8</a> or visit the <a href="https://988lifeline.org/"target="_blank">988 Lifeline website</a>.</p>
+	<p>If you are in a crisis, please <em>do not</em> contact me. Instead call or text <a href="tel:988" className={linkStyle}>9-8-8</a> or visit the <a href="https://988lifeline.org/"target="_blank" className={linkStyle}>988 Lifeline website</a>.</p>
       </section>
-
-
-
     </main>
   )
 }
