49cf1fc6e77729b0fc10fac9c6beed65c3449fbc
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-29 12:25:58 -0700

    Fix logo import
    Since I reorganized the project to use src, the import statement broke
    for the logo.  Thus, I change the src for the logo Image to come
    directly from the public folder.

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
-import logo from '@/public/wpc-logo.svg'
 
 export default function Header() {
   return (
     <header className="flex items-center gap-4 mb-8">
       <Image
-	src={logo}
+	src="wpc-logo.svg"
 	alt='Logo of Whole Presence Counseling, which resembles a hand illustration of a thick, imperfect black circle.'
 	className="w-12 md:w-16"
+	width={1372.047241}
+	height={1372.047241}
       />
       <h1 className="text-2xl md:text-4xl leading-none font-bold">Whole Presence Counseling</h1>
     </header>
